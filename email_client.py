e3d2d8ee38e468df17bc18538a47ee53a810f457
Adam Stueckrath <stueckrath.adam@gmail.com>
2019-04-06 10:56:55 -0500

    added Emailer parameter for using terminal to login

--- email_client.py
+++ email_client.py
@@ -2,13 +2,13 @@ import os
 import getpass
 import datetime
 import smtplib
-from email.MIMEBase import MIMEBase
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.encoders import encode_base64
 from email.mime.multipart import MIMEMultipart
 
-class Email():
-    def __init__(self, config=None, delay_login=True):
+class Emailer():
+    def __init__(self, config=None, delay_login=True, from_input=False):
         """
         The resolution order for the following information is
         config -> environment variable -> guess
@@ -43,34 +43,39 @@ class Email():
         if config is None:
             config = {}
 
-        self._sender_email = config.get('sender_email', os.getenv('EMAIL_ADDRESS'))
-        if not self._sender_email:
-            raise ValueError('Either config must contain "sender_email" or the '
-                             'EMAIL_ADDRESS environment variable must '
-                             'be set.')
+        if from_input:
+            self.from_login()
+        else:
+            self._sender_email = config.get('sender_email', os.getenv('EMAIL_ADDRESS'))
+            if not self._sender_email:
+                raise ValueError('Either config must contain "sender_email" or the '
+                                 'EMAIL_ADDRESS environment variable must '
+                                 'be set.')
 
-        self._password = config.get('password', os.getenv('EMAIL_PASSWORD'))
-        if not self._password:
-            raise ValueError('Either config must contain "password" or the '
-                             'EMAIL_PASSWORD environment variable must '
-                             'be set.')
+            self._password = config.get('password', os.getenv('EMAIL_PASSWORD'))
+            if not self._password:
+                raise ValueError('Either config must contain "password" or the '
+                                 'EMAIL_PASSWORD environment variable must '
+                                 'be set.')
 
-        self._port = config.get('port', os.getenv('EMAIL_PORT'))
-        if self._port is None:
-            self._port = 587
+            self._port = config.get('port', os.getenv('EMAIL_PORT'))
+            if self._port is None:
+                self._port = 587
 
-        self._host = config.get('host', os.getenv('EMAIL_HOST'))
-        if self._host is None:
-            if '@outlook.com' in self._sender_email:
-                self._host = 'smtp.office365.com'
-            elif '@gmail.com' in self._sender_email:
-                self._host = 'smtp.gmail.com'
-            else:
-                raise ValueError('Cannot guess host given email.')
+            self._host = config.get('host', os.getenv('EMAIL_HOST'))
+            if self._host is None:
+                if '@outlook.com' or '@hotmail.com' in self._sender_email:
+                    self._host = 'smtp.office365.com'
+                elif '@gmail.com' in self._sender_email:
+                    self._host = 'smtp.gmail.com'
+                elif '@yahoo.com' in self._sender_email:
+                    self._host = 'smtp.mail.yahoo.com'
+                else:
+                    raise ValueError('Cannot guess host given email.')
 
-        self._logged_in = False
-        if not delay_login:
-            self._login()
+            self._logged_in = False
+            if not delay_login:
+                self._login()
 
     def _login(self):
         self._smtp = smtplib.SMTP(host=self._host, port=self._port, timeout=10)
@@ -82,10 +87,10 @@ class Email():
     def from_login(**kwargs):
         """
         Get prompted for login information at your command line.
-        All keyword args are passed to the initializer Email().
+        All keyword args are passed to the initializer Emailer().
         """
         config = {}
-        config['sender_email'] = raw_input('Email account to send from: ')
+        config['sender_email'] = input('Email account to send from: ')
         if '@outlook.com' or '@hotmail.com' in config['sender_email']:
             port_message = 'Port # (likely 587): '
             host_message = 'Host URL (likely smtp.office365.com): '
@@ -98,12 +103,12 @@ class Email():
         else:
             port_message = 'Port #: '
             host_message = 'Host URL: '
-        config['port'] = int(raw_input(port_message))
-        config['host'] = raw_input(host_message)
+        config['port'] = int(input(port_message))
+        config['host'] = input(host_message)
         config['password'] = getpass.getpass(
             'Email password (nothing will be shown as you type):'
         )
-        return Email(config=config, **kwargs)
+        return Emailer(config=config, **kwargs)
 
     def send_email(self, destinations, subject, text, files=None):
         """
@@ -161,3 +166,9 @@ class Email():
 
         return
 
+
+if __name__ == '__main__':
+    try:
+        Emailer() or Emailer().from_login()
+    except:
+        raise
