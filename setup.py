8705d097b1c9bac409f2555108d1e9df19f28930
Adam Stueckrath <stueckrath.adam@gmail.com>
2019-04-27 13:41:00 -0500

    renamed package to AutoEmailer

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
-import Emailer
+import AutoEmailer
 
-setup(name='Emailer',
-      version=Emailer.__version__,
+setup(name='AutoEmailer',
+      version=AutoEmailer.__version__,
       description='Email client to make emails easier.',
       author='Adam Stueckrath',
       author_email='stueckrath.adam@gmail.com',
       url='',
-      packages=['Emailer'],
+      packages=['AutoEmailer'],
       install_requires=[],
       tests_require=[],
       package_data={}
