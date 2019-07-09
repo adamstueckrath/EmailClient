f7bbff1807d1b6e9ba2389ea6e890bb11f55171e
Adam Stueckrath <stueckrath.adam@gmail.com>
2019-07-08 20:16:29 -0500

    working through adding config.py; updated init to import config; removed credentials from emailer.py

--- AutoEmailer/__init__.py
+++ AutoEmailer/__init__.py
@@ -1,3 +1,4 @@
 from .emailer import Emailer
+from .config import EmailerConfig
 
 __version__ = '0.0.1'
--- AutoEmailer/emailer.py
+++ AutoEmailer/emailer.py
@@ -2,6 +2,7 @@ import os
 import getpass
 import datetime
 import smtplib
+from AutoEmailer import EmailerConfig
 from pathlib import Path
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
@@ -10,66 +11,13 @@ from email.mime.multipart import MIMEMultipart
 
 
 class Emailer:
+    """
+    Welcome to the Auto Emailer to send all of your emails!
+    """
     def __init__(self, config=None, delay_login=True):
         """
-        Welcome to the Auto Emailer to send all of your emails!
-
-        Order for the following information is
-        config -> environment variable -> guess
-        :param config: dict
-            sender_email : str
-              - sender email.
-              - equivalent environment variable: EMAIL_ADDRESS
-            password : str
-              - password of that email account.
-              - equivalent environment variable: EMAIL_PASSWORD
-            port : int
-              - port number.
-              - equivalent environment variable: EMAIL_PORT
-              - If neither config nor environment variable is set,
-                then we attempt to guess the port number from the
-                sender_email.
-            host : str
-              - domain name of host.
-              - equivalent environment variable: EMAIL_HOST
-              - If neither config nor environment variable is set,
-                then we attempt to guess the host from the
-                sender_email.
-        :delay_login: bool
-            if True, no login attempt will be made until send_mail
-            is called. Otherwise, a login attempt will be made
-            at construction time.
         """
-        if config is None:
-            config = {}
-
-        self._sender_email = config.get('sender_email', os.environ.get('EMAIL_ADDRESS'))
-        if not self._sender_email:
-            raise ValueError('Either config must contain "sender_email" or the '
-                             'EMAIL_ADDRESS environment variable must '
-                             'be set.')
-
-        self._password = config.get('password', os.environ.get('EMAIL_PASSWORD'))
-        if not self._password:
-            raise ValueError('Either config must contain "password" or the '
-                             'EMAIL_PASSWORD environment variable must '
-                             'be set.')
-
-        self._port = config.get('port', os.getenv('EMAIL_PORT'))
-        if self._port is None:
-            self._port = 587
-
-        self._host = config.get('host', os.getenv('EMAIL_HOST'))
-        if self._host is None:
-            if ('@outlook.com' in self._sender_email) or ('@hotmail.com' in self._sender_email):
-                self._host = 'smtp.office365.com'
-            elif '@gmail.com' in self._sender_email:
-                self._host = 'smtp.gmail.com'
-            elif '@yahoo.com' in self._sender_email:
-                self._host = 'smtp.mail.yahoo.com'
-            else:
-                raise ValueError('Cannot guess host given email.')
-
+        self._config = EmailerConfig(**config) if config else EmailerConfig()
         self._logged_in = False
         if not delay_login:
             self._login()
@@ -78,7 +26,7 @@ class Emailer:
         """
         Use the config from input or environment variables to login.
         """
-        self._smtp = smtplib.SMTP(host=self._host, port=self._port, timeout=10)
+        self._smtp = smtplib.SMTP(host=self._config, port=self._port, timeout=10)
         self._smtp.starttls()
         self._smtp.login(self._sender_email, self._password)
         self._logged_in = True
