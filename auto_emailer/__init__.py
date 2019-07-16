5d112dc180a6e66aed8151a79c5377a70427f577
Adam Stueckrath <stueckrath.adam@gmail.com>
2019-07-16 18:55:27 -0500

    removed version from __init__ and moved to setup.py; added install_requires arguments in setup.py;

--- auto_emailer/__init__.py
+++ auto_emailer/__init__.py
@@ -1,4 +1,2 @@
 from .emailer import Emailer
 from .config import credentials, default, environment_vars
-
-__version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='AutoEmailer',
+setup(name='auto_emailer',
       version='0.0.1',
       description='Email client to automate emails.',
       author='Adam Stueckrath',
       author_email='stueckrath.adam@gmail.com',
       url='',
-      packages=['AutoEmailer'],
-      install_requires=[],
+      packages=['auto_emailer'],
+      install_requires=['six>=1.9.0'],
       tests_require=[],
       package_data={}
       )
