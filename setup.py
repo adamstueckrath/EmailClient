4e456d48be41c3c523d60eadbc94ccfdd471c278
Adam <stueckrath.adam@gmail.com>
2019-08-14 20:12:39 -0500

    updated gitignore for sdist; updated setup.py; (#10)

--- setup.py
+++ setup.py
@@ -5,9 +5,28 @@ setup(name='auto_emailer',
       description='auto-emailer library for Python',
       author='Adam Stueckrath',
       author_email='stueckrath.adam@gmail.com',
-      url='',
+      url='https://github.com/adamstueckrath/auto-emailer',
       packages=['auto_emailer'],
       install_requires=['six>=1.9.0'],
       tests_require=['six>=1.9.0'],
-      package_data={}
+      keywords='smtp email',
+      license='MIT',
+      classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            "Development Status :: 3 - Alpha",
+
+            # Indicate who your project is intended for
+            "Intended Audience :: Developers",
+            'Topic :: Software Development :: Libraries :: Python Modules',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: MIT License',
+
+            # Specify the Python versions you support here.
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.6.5",
+            "Programming Language :: Python :: 3.7"]
       )
